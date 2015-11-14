78b20f64babecd51941c6711c5d17be75a460c65
Christian Nagel <christian@christiannagel.com>
2015-11-14 19:44:57 +0100

    initial commit - WPF sample for huge TreeView

---
+++ TreeViewSample/TreeViewSample/NodeFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreeViewSample
+{
+    public class NodeFactory
+    {
+        public IEnumerable<ANode> GetNodes()
+        {
+            return Enumerable.Range(0, 10).Select(x => new ANode($"{x}")).ToList();
+        }
+    }
+}
